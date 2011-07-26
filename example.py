e82e0e42f6549364522a2257ca06ee90690e43d3
Adrian Sampson <adrian@radbox.org>
2011-07-25 17:33:14 -0700

    map() works!

--- example.py
+++ example.py
@@ -3,14 +3,19 @@ import execnet
 import sys
 import textwrap
 
+# Shared "worker" functions.
+def square(n):
+    return n * n
+def pid():
+    import os
+    return os.getpid()
+
 def example_1():
     """
     Simplest possible example: run a small computation in local
     interpreters.
     """
     group = execnet.Group(['popen'] * 2)
-    def square(n):
-        return n * n
     with enf.GatewayExecutor(group) as executor:
         futures = [executor.submit(square, n) for n in range(5)]
         for future in futures:
@@ -22,13 +27,20 @@ def example_2():
     is actually forking them.
     """
     group = execnet.Group(['popen'] * 3)
-    def pid():
-        import os
-        return os.getpid()
     with enf.GatewayExecutor(group) as executor:
         futures = [executor.submit(pid) for i in range(10)]
         for future in futures:
             print future.result()
+
+def example_3():
+    """
+    Use concurrent.futures' implementation of map() to concisely
+    run DOALL computations.
+    """
+    group = execnet.Group(['popen'] * 2)
+    with enf.GatewayExecutor(group) as executor:
+        for res in executor.map(square, range(10)):
+            print res
 
 if __name__ == '__main__':
     for arg in sys.argv[1:]:
